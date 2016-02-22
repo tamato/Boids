b280b9aaeb759f7610cb811df0a32b85b933153c
terence amato <terence.amato@gmail.com>
2016-02-21 19:56:59 -0700

    added an additional layer of tessalte to help with anitalaising at a later time

--- common/curvegenerator.cpp
+++ common/curvegenerator.cpp
@@ -25,12 +25,11 @@ void CurveGenerator::generate()
     const std::vector<float>& radiusList = LineSegments[0].Radii;
 
     const uint32_t ptls_size = point_list.size();
-    const uint32_t pts_per_line = 2;
+    const uint32_t pts_per_line = 4;
     const uint32_t pt_count = ptls_size * pts_per_line;
-    const uint32_t verts_per_quad = 6;
-    const std::vector<uint32_t> base_indicies = { 0, 1, 3, 0, 3, 2 };
+    const uint32_t verts_per_strip = 18;
 
-    Indices.resize((ptls_size-1) * verts_per_quad);
+    Indices.resize((ptls_size-1) * verts_per_strip);
     Positions.resize(pt_count);
     PrevPoint.resize(pt_count);
     NextPoint.resize(pt_count);
@@ -44,23 +43,51 @@ void CurveGenerator::generate()
         // fill out vert positions
         uint32_t pt_idx = idx * pts_per_line;
         Positions[pt_idx+0] = point_list[idx];
-        PrevPoint[pt_idx+0] = glm::vec4(point_list[line_idx-1], +radiusList[idx]);
-        NextPoint[pt_idx+0] = glm::vec4(point_list[line_idx+1], +radiusList[idx]);
+        PrevPoint[pt_idx+0] = glm::vec4(point_list[line_idx-1], +(50.1f+radiusList[idx]));
+        NextPoint[pt_idx+0] = glm::vec4(point_list[line_idx+1], +(50.1f+radiusList[idx]));
 
         Positions[pt_idx+1] = point_list[idx];
-        PrevPoint[pt_idx+1] = glm::vec4(point_list[line_idx-1], -radiusList[idx]);
-        NextPoint[pt_idx+1] = glm::vec4(point_list[line_idx+1], -radiusList[idx]);
+        PrevPoint[pt_idx+1] = glm::vec4(point_list[line_idx-1], +radiusList[idx]);
+        NextPoint[pt_idx+1] = glm::vec4(point_list[line_idx+1], +radiusList[idx]);
+
+        Positions[pt_idx+2] = point_list[idx];
+        PrevPoint[pt_idx+2] = glm::vec4(point_list[line_idx-1], -radiusList[idx]);
+        NextPoint[pt_idx+2] = glm::vec4(point_list[line_idx+1], -radiusList[idx]);
+
+        Positions[pt_idx+3] = point_list[idx];
+        PrevPoint[pt_idx+3] = glm::vec4(point_list[line_idx-1], -(50.1f+radiusList[idx]));
+        NextPoint[pt_idx+3] = glm::vec4(point_list[line_idx+1], -(50.1f+radiusList[idx]));
+
 
         // fill out indices that will make our quads that make up the line
-        uint32_t vert_idx = idx * verts_per_quad;
+        uint32_t vert_idx = idx * verts_per_strip;
         
         if (idx == ptls_size-1) continue;
-        Indices[vert_idx+0] = base_indicies[0] + pt_idx;
-        Indices[vert_idx+1] = base_indicies[1] + pt_idx;
-        Indices[vert_idx+2] = base_indicies[2] + pt_idx;
+        // left edge
+        Indices[vert_idx+ 0] = 0 + pt_idx;
+        Indices[vert_idx+ 1] = 1 + pt_idx;
+        Indices[vert_idx+ 2] = 5 + pt_idx;
 
-        Indices[vert_idx+3] = base_indicies[3] + pt_idx;
-        Indices[vert_idx+4] = base_indicies[4] + pt_idx;
-        Indices[vert_idx+5] = base_indicies[5] + pt_idx;
+        Indices[vert_idx+ 3] = 0 + pt_idx;
+        Indices[vert_idx+ 4] = 5 + pt_idx;
+        Indices[vert_idx+ 5] = 4 + pt_idx;
+
+        // middle
+        Indices[vert_idx+ 6] = 1 + pt_idx;
+        Indices[vert_idx+ 7] = 2 + pt_idx;
+        Indices[vert_idx+ 8] = 6 + pt_idx;
+
+        Indices[vert_idx+ 9] = 1 + pt_idx;
+        Indices[vert_idx+10] = 6 + pt_idx;
+        Indices[vert_idx+11] = 5 + pt_idx;
+
+        // right edge
+        Indices[vert_idx+12] = 2 + pt_idx;
+        Indices[vert_idx+13] = 3 + pt_idx;
+        Indices[vert_idx+14] = 7 + pt_idx;
+
+        Indices[vert_idx+15] = 2 + pt_idx;
+        Indices[vert_idx+16] = 7 + pt_idx;
+        Indices[vert_idx+17] = 6 + pt_idx;
     }
 }
